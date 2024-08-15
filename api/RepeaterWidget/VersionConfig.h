eb459f94da520a017e439dfa16ab4d3c82bc2b05
xtx <2656823393@qq.com>
2024-08-15 22:45:00 +0800

    1

--- api/RepeaterWidget/VersionConfig.h
+++ api/RepeaterWidget/VersionConfig.h
@@ -1,3 +1,3 @@
 //VersionConfig.h.in
-#define V_BUILD_TIME "20240813_235256"
-#define V_GIT_INFO "Check-action_dbbc170"
+#define V_BUILD_TIME "20240815_224300"
+#define V_GIT_INFO "Check-action_f915157"
