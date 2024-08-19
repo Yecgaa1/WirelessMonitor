68595b14ad7709fad2c07335f39b6d89df696a8e
xutongxin <2656823393@qq.com>
2024-08-19 19:59:14 +0800

    1

--- api/RepeaterWidget/VersionConfig.h
+++ api/RepeaterWidget/VersionConfig.h
@@ -1,3 +1,3 @@
 //VersionConfig.h.in
-#define V_BUILD_TIME "20240819_183957"
-#define V_GIT_INFO "Check-action_5d7d472"
+#define V_BUILD_TIME "20240819_195639"
+#define V_GIT_INFO "Check-action_0262b9d"
