36fbdf2588e29fadafe522254f5ed0acccd9a4ac
xutongxin <2656823393@qq.com>
2024-08-21 01:43:52 +0800

    奇怪的完成了打包

--- api/RepeaterWidget/VersionConfig.h
+++ api/RepeaterWidget/VersionConfig.h
@@ -1,3 +1,3 @@
 //VersionConfig.h.in
-#define V_BUILD_TIME "20240819_195639"
-#define V_GIT_INFO "Check-action_0262b9d"
+#define V_BUILD_TIME "20240821_013925"
+#define V_GIT_INFO "Check-action_c961438"
