caaa4ef1a4b3f2fb7d2e7cd72476fc441fef73c9
xutongxin <2656823393@qq.com>
2024-08-23 02:31:44 +0800

    打包名组建

--- api/RepeaterWidget/VersionConfig.h
+++ api/RepeaterWidget/VersionConfig.h
@@ -1,3 +1,3 @@
 //VersionConfig.h.in
-#define V_BUILD_TIME "20240822_220207"
-#define V_GIT_INFO "Check-action_121c0a4"
+#define V_BUILD_TIME "20240822_223324"
+#define V_GIT_INFO "Check-action_99252bd"
