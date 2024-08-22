99252bd86d07deadc0233a4907683c076c0b613b
xutongxin <2656823393@qq.com>
2024-08-22 22:09:40 +0800

    完成分别打包

--- api/RepeaterWidget/VersionConfig.h
+++ api/RepeaterWidget/VersionConfig.h
@@ -1,3 +1,3 @@
 //VersionConfig.h.in
-#define V_BUILD_TIME "20240821_013925"
-#define V_GIT_INFO "Check-action_c961438"
+#define V_BUILD_TIME "20240822_220207"
+#define V_GIT_INFO "Check-action_121c0a4"
