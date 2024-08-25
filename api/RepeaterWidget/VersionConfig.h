1bbef89499f662a3ca8dc70ab7d7812b60693b41
xtx <2656823393@qq.com>
2024-08-25 19:22:43 +0800

    1

--- api/RepeaterWidget/VersionConfig.h
+++ api/RepeaterWidget/VersionConfig.h
@@ -1,3 +1,5 @@
 //VersionConfig.h.in
-#define V_BUILD_TIME "20240824_1255"
-#define V_GIT_INFO "Check-action_f011e6c"
+#define V_BIG_VERSION "0.1.1"
+#define V_BAT_VERSION "0.1.1.20240825.1850"
+#define V_BUILD_TIME "20240825.1850"
+#define V_GIT_INFO "_"
